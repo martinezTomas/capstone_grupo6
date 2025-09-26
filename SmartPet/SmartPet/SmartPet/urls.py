2fea3ef92c4019426a48e6f4a05c62e7e852b7ba
kripilope <llopezllucas@gmail.com>
2025-09-26 18:02:50 -0300

    Pagina de inicio con django funcional

--- SmartPet/SmartPet/SmartPet/urls.py
+++ SmartPet/SmartPet/SmartPet/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from usuarios import views as usuarios_views
 
 urlpatterns = [
+    path('', usuarios_views.index, name='home'),
     path('admin/', admin.site.urls),
+    path('usuarios/', include('usuarios.urls')),
+    
 ]
